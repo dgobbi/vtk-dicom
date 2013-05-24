6a2eb30836b523811954841a628838fc23080311
David Gobbi <david.gobbi@gmail.com>
2013-05-24 13:51:29 -0600

    Add a test for vtkDICOMVM, fix typos in the header files.

---
+++ Testing/TestDICOMVM.cxx
@@ -0,0 +1,163 @@
+#include "vtkDICOMVM.h"
+#include "vtkDICOMValue.h"
+
+#include <sstream>
+
+#include <string.h>
+#include <stdlib.h>
+
+// macro for performing tests
+#define TestAssert(t) \
+if (!(t)) \
+{ \
+  cout << exename << ": Assertion Failed: " << #t << "\n"; \
+  cout << __FILE__ << ":" << __LINE__ << "\n"; \
+  cout.flush(); \
+  rval |= 1; \
+}
+
+int main(int argc, char *argv[])
+{
+  int rval = 0;
+  const char *exename = argv[0];
+
+  // remove path portion of exename
+  const char *cp = exename + strlen(exename);
+  while (cp != exename && cp[-1] != '\\' && cp[-1] != '/') { --cp; }
+  exename = cp;
+
+  { // Test default constructor.
+  vtkDICOMVM vm;
+  TestAssert(!vm.IsValid());
+  }
+
+  { // Test the individual VMs
+  vtkDICOMVM vmM1(vtkDICOMVM::M1);
+  TestAssert(vmM1 == vtkDICOMVM::M1);
+  TestAssert(vmM1.GetMin() == 1);
+  TestAssert(vmM1.GetMax() == 1);
+  TestAssert(vmM1.GetStep() == 1);
+
+  vtkDICOMVM vmM1T2(vtkDICOMVM::M1T2);
+  TestAssert(vmM1T2 == vtkDICOMVM::M1T2);
+  TestAssert(vmM1T2.GetMin() == 1);
+  TestAssert(vmM1T2.GetMax() == 2);
+  TestAssert(vmM1T2.GetStep() == 1);
+
+  vtkDICOMVM vmM1T3(vtkDICOMVM::M1T3);
+  TestAssert(vmM1T3 == vtkDICOMVM::M1T3);
+  TestAssert(vmM1T3.GetMin() == 1);
+  TestAssert(vmM1T3.GetMax() == 3);
+  TestAssert(vmM1T3.GetStep() == 1);
+
+  vtkDICOMVM vmM1T32(vtkDICOMVM::M1T32);
+  TestAssert(vmM1T32 == vtkDICOMVM::M1T32);
+  TestAssert(vmM1T32.GetMin() == 1);
+  TestAssert(vmM1T32.GetMax() == 32);
+  TestAssert(vmM1T32.GetStep() == 1);
+
+  vtkDICOMVM vmM1T99(vtkDICOMVM::M1T99);
+  TestAssert(vmM1T99 == vtkDICOMVM::M1T99);
+  TestAssert(vmM1T99.GetMin() == 1);
+  TestAssert(vmM1T99.GetMax() == 99);
+  TestAssert(vmM1T99.GetStep() == 1);
+
+  vtkDICOMVM vmM1TN(vtkDICOMVM::M1TN);
+  TestAssert(vmM1TN == vtkDICOMVM::M1TN);
+  TestAssert(vmM1TN.GetMin() == 1);
+  TestAssert(vmM1TN.GetMax() == -1);
+  TestAssert(vmM1TN.GetStep() == 1);
+
+  vtkDICOMVM vmM2(vtkDICOMVM::M2);
+  TestAssert(vmM2 == vtkDICOMVM::M2);
+  TestAssert(vmM2.GetMin() == 2);
+  TestAssert(vmM2.GetMax() == 2);
+  TestAssert(vmM2.GetStep() == 1);
+
+  vtkDICOMVM vmM2T2N(vtkDICOMVM::M2T2N);
+  TestAssert(vmM2T2N == vtkDICOMVM::M2T2N);
+  TestAssert(vmM2T2N.GetMin() == 2);
+  TestAssert(vmM2T2N.GetMax() == -1);
+  TestAssert(vmM2T2N.GetStep() == 2);
+
+  vtkDICOMVM vmM2TN(vtkDICOMVM::M2TN);
+  TestAssert(vmM2TN == vtkDICOMVM::M2TN);
+  TestAssert(vmM2TN.GetMin() == 2);
+  TestAssert(vmM2TN.GetMax() == -1);
+  TestAssert(vmM2TN.GetStep() == 1);
+
+  vtkDICOMVM vmM3(vtkDICOMVM::M3);
+  TestAssert(vmM3 == vtkDICOMVM::M3);
+  TestAssert(vmM3.GetMin() == 3);
+  TestAssert(vmM3.GetMax() == 3);
+  TestAssert(vmM3.GetStep() == 1);
+
+  vtkDICOMVM vmM3T3N(vtkDICOMVM::M3T3N);
+  TestAssert(vmM3T3N == vtkDICOMVM::M3T3N);
+  TestAssert(vmM3T3N.GetMin() == 3);
+  TestAssert(vmM3T3N.GetMax() == -1);
+  TestAssert(vmM3T3N.GetStep() == 3);
+
+  vtkDICOMVM vmM3TN(vtkDICOMVM::M3TN);
+  TestAssert(vmM3TN == vtkDICOMVM::M3TN);
+  TestAssert(vmM3TN.GetMin() == 3);
+  TestAssert(vmM3TN.GetMax() == -1);
+  TestAssert(vmM3TN.GetStep() == 1);
+
+  vtkDICOMVM vmM4(vtkDICOMVM::M4);
+  TestAssert(vmM4 == vtkDICOMVM::M4);
+  TestAssert(vmM4.GetMin() == 4);
+  TestAssert(vmM4.GetMax() == 4);
+  TestAssert(vmM4.GetStep() == 1);
+
+  vtkDICOMVM vmM5(vtkDICOMVM::M5);
+  TestAssert(vmM5 == vtkDICOMVM::M5);
+  TestAssert(vmM5.GetMin() == 5);
+  TestAssert(vmM5.GetMax() == 5);
+  TestAssert(vmM5.GetStep() == 1);
+
+  vtkDICOMVM vmM6(vtkDICOMVM::M6);
+  TestAssert(vmM6 == vtkDICOMVM::M6);
+  TestAssert(vmM6.GetMin() == 6);
+  TestAssert(vmM6.GetMax() == 6);
+  TestAssert(vmM6.GetStep() == 1);
+
+  vtkDICOMVM vmM6TN(vtkDICOMVM::M6TN);
+  TestAssert(vmM6TN == vtkDICOMVM::M6TN);
+  TestAssert(vmM6TN.GetMin() == 6);
+  TestAssert(vmM6TN.GetMax() == -1);
+  TestAssert(vmM6TN.GetStep() == 1);
+
+  vtkDICOMVM vmM8(vtkDICOMVM::M8);
+  TestAssert(vmM8 == vtkDICOMVM::M8);
+  TestAssert(vmM8.GetMin() == 8);
+  TestAssert(vmM8.GetMax() == 8);
+  TestAssert(vmM8.GetStep() == 1);
+
+  vtkDICOMVM vmM9(vtkDICOMVM::M9);
+  TestAssert(vmM9 == vtkDICOMVM::M9);
+  TestAssert(vmM9.GetMin() == 9);
+  TestAssert(vmM9.GetMax() == 9);
+  TestAssert(vmM9.GetStep() == 1);
+
+  vtkDICOMVM vmM16(vtkDICOMVM::M16);
+  TestAssert(vmM16 == vtkDICOMVM::M16);
+  TestAssert(vmM16.GetMin() == 16);
+  TestAssert(vmM16.GetMax() == 16);
+  TestAssert(vmM16.GetStep() == 1);
+
+  vtkDICOMVM vmM24(vtkDICOMVM::M24);
+  TestAssert(vmM24 == vtkDICOMVM::M24);
+  TestAssert(vmM24.GetMin() == 24);
+  TestAssert(vmM24.GetMax() == 24);
+  TestAssert(vmM24.GetStep() == 1);
+
+  vtkDICOMVM vmM256(vtkDICOMVM::M256);
+  TestAssert(vmM256 == vtkDICOMVM::M256);
+  TestAssert(vmM256.GetMin() == 256);
+  TestAssert(vmM256.GetMax() == 256);
+  TestAssert(vmM256.GetStep() == 1);
+  }
+
+  return rval;
+}
--- vtkDICOMVM.h
+++ vtkDICOMVM.h
@@ -26,14 +26,14 @@ public:
     M1    = 0x0101,
     M1T2  = 0x0102,
     M1T3  = 0x0103,
-    M1T32 = 0x0132,
+    M1T32 = 0x0120,
     M1T99 = 0x0163,
     M1TN  = 0x01FF,
     M2    = 0x0202,
     M2T2N = 0x02FE,
     M2TN  = 0x02FF,
     M3    = 0x0303,
-    M3T3N = 0x03FC,
+    M3T3N = 0x03FD,
     M3TN  = 0x03FF,
     M4    = 0x0404,
     M5    = 0x0505,
@@ -57,17 +57,17 @@ public:
 
   //! Get the minimum allowed number of values
   int GetMin() const {
-    if ((this->Key & 0x8000) != 0) { return (this->Key & 0x7fff); } 
+    if ((this->Key & 0x8000) != 0) { return (this->Key & 0x7fff); }
     else { return (this->Key >> 8); } }
 
   //! Get the maximum allowed number of values, -1 if unlimited
   int GetMax() const {
-    if ((this->Key & 0x8000) != 0) { return (this->Key & 0x7fff); } 
+    if ((this->Key & 0x8000) != 0) { return (this->Key & 0x7fff); }
     else { return ((this->Key & 0x80) == 0 ? (this->Key & 0xff) : -1); } }
 
   //! Get the step between allowed values.
   int GetStep() const {
-    if ((this->Key & 0x8000) != 0) { return 1; } 
+    if ((this->Key & 0x8000) != 0) { return 1; }
     else { return ((this->Key & 0x80) == 0 ? 1 : ((-this->Key) & 0xff)); } }
 
   bool operator==(vtkDICOMVM b) const { return (this->Key == b.Key); }
