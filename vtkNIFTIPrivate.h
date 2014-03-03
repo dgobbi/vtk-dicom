1daf30a638903c6fa8a982fb967a6acca248fc51
David Gobbi <david.gobbi@gmail.com>
2014-03-03 09:58:47 -0700

    The nifti2 header int64_t is not available on all compilers.
    The stdint.h header is available in C99 and C++11, but is absent
    on all but the very latest MSVC.

--- vtkNIFTIPrivate.h
+++ vtkNIFTIPrivate.h
@@ -84,57 +84,6 @@ struct nifti_1_header { /* NIFTI-1 usage         */  /*  offset  */
 
 typedef struct nifti_1_header nifti_1_header;
 
-
-/*! \struct nifti_2_header
-    \brief Data structure defining the fields in the nifti2 header.
-           This binary header should be found at the beginning of a valid
-           NIFTI-2 header file.
- */
-
-                        /*************************/  /************/
-struct nifti_2_header { /* NIFTI-2 usage         */  /*  offset  */
-                        /*************************/  /************/
-  int   sizeof_hdr;     /*!< MUST be 540           */   /*   0 */
-  char  magic[8];       /*!< MUST be valid signature. */ /*   4 */
-  short datatype;       /*!< Defines data type!    */   /*  12 */
-  short bitpix;         /*!< Number bits/voxel.    */   /*  14 */
-  int64_t dim[8];       /*!< Data array dimensions.*/   /*  16 */
-  double intent_p1;     /*!< 1st intent parameter. */   /*  80 */
-  double intent_p2;     /*!< 2nd intent parameter. */   /*  88 */
-  double intent_p3;     /*!< 3rd intent parameter. */   /*  96 */
-  double pixdim[8];     /*!< Grid spacings.        */   /* 104 */
-  int64_t vox_offset;   /*!< Offset into .nii file */   /* 168 */
-  double scl_slope;     /*!< Data scaling: slope.  */   /* 176 */
-  double scl_inter;     /*!< Data scaling: offset. */   /* 184 */
-  double cal_max;       /*!< Max display intensity */   /* 192 */
-  double cal_min;       /*!< Min display intensity */   /* 200 */
-  double slice_duration;/*!< Time for 1 slice.     */   /* 208 */
-  double toffset;       /*!< Time axis shift.      */   /* 216 */
-  int64_t slice_start;  /*!< First slice index.    */   /* 224 */
-  int64_t slice_end;    /*!< Last slice index.     */   /* 232 */
-  char descrip[80];     /*!< any text you like.    */   /* 240 */
-  char aux_file[24];    /*!< auxiliary filename.   */   /* 320 */
-  int qform_code;       /*!< NIFTI_XFORM_* code.   */   /* 344 */
-  int sform_code;       /*!< NIFTI_XFORM_* code.   */   /* 348 */
-  double quatern_b;     /*!< Quaternion b param.   */   /* 352 */
-  double quatern_c;     /*!< Quaternion c param.   */   /* 360 */
-  double quatern_d;     /*!< Quaternion d param.   */   /* 368 */
-  double qoffset_x;     /*!< Quaternion x shift.   */   /* 376 */
-  double qoffset_y;     /*!< Quaternion y shift.   */   /* 384 */
-  double qoffset_z;     /*!< Quaternion z shift.   */   /* 392 */
-  double srow_x[4];     /*!< 1st row affine transform. */ /* 400 */
-  double srow_y[4];     /*!< 2nd row affine transform. */ /* 432 */
-  double srow_z[4];     /*!< 3rd row affine transform. */ /* 464 */
-  int slice_code;       /*!< Slice timing order.   */   /* 496 */
-  int xyzt_units;       /*!< Units of pixdim[1..4] */   /* 500 */
-  int intent_code;      /*!< NIFTI_INTENT_* code.  */   /* 504 */
-  char intent_name[16]; /*!< 'name' or meaning of data. */  /* 508 */
-  char dim_info;        /*!< MRI slice ordering.   */   /* 524 */
-  char unused_str[15];  /*!< unused, filled with \0 */  /* 525 */
-};                    /**** 540 bytes total ****/
-
-typedef struct nifti_2_header nifti_2_header;
-
 /*---------------------------------------------------------------------------*/
 /* TYPE OF DATA (acceptable values for datatype field):
    ---------------------------------------------------
@@ -226,4 +175,103 @@ typedef struct nifti_2_header nifti_2_header;
 /*=================*/
 
 #endif /* _NIFTI_HEADER_ */
+
+#ifndef __NIFTI2_HEADER
+#define __NIFTI2_HEADER
+
+/*---------------------------------------------------------------------------*/
+/* Changes to the header from NIFTI-1 to NIFTI-2 are intended to allow for
+   larger and more accurate fields.  The changes are as follows:
+
+      - short dim[8]         -> int64_t dim[8]
+      - float intent_p1,2,3  -> double intent_p1,2,3    (3 fields)
+      - float pixdim[8]      -> double pixdim[8]
+      - float vox_offset     -> int64_t vox_offset
+      - float scl_slope      -> double scl_slope
+      - float scl_inter      -> double scl_inter
+      - float cal_max        -> double cal_max
+      - float cal_min        -> double cal_min
+      - float slice_duration -> double slice_duration
+      - float toffset        -> double toffset
+      - short slice_start    -> int64_t slice_start
+      - short slice_end      -> int64_t slice_end
+      - char slice_code      -> int32_t slice_code
+      - char xyzt_units      -> int32_t xyzt_units
+      - short intent_code    -> int32_t intent_code
+      - short qform_code     -> int32_t qform_code
+      - short sform_code     -> int32_t sform_code
+      - float quatern_b,c,d  -> double quatern_b,c,d    (3 fields)
+      - float srow_x,y,z[4]  -> double srow_x,y,z[4]    (3 fields)
+      - char magic[4]        -> char magic[8]
+      - char unused_str[15]  -> padding added at the end of the header
+
+      - previously unused fields have been removed:
+           data_type, db_name, extents, session_error, regular, glmax, glmin
+
+      - the field ordering has been changed
+-----------------------------------------------------------------------------*/
+
+/*=================*/
+#ifdef  __cplusplus
+extern "C" {
+#endif
+/*=================*/
+
+/*! \struct nifti_2_header
+    \brief Data structure defining the fields in the nifti2 header.
+           This binary header should be found at the beginning of a valid
+           NIFTI-2 header file.
+ */
+
+                        /*************************/  /************/
+struct nifti_2_header { /* NIFTI-2 usage         */  /*  offset  */
+                        /*************************/  /************/
+  int   sizeof_hdr;     /*!< MUST be 540           */   /*   0 */
+  char  magic[8];       /*!< MUST be valid signature. */ /*   4 */
+  short datatype;       /*!< Defines data type!    */   /*  12 */
+  short bitpix;         /*!< Number bits/voxel.    */   /*  14 */
+  long long dim[8];     /*!< Data array dimensions.*/   /*  16 */
+  double intent_p1;     /*!< 1st intent parameter. */   /*  80 */
+  double intent_p2;     /*!< 2nd intent parameter. */   /*  88 */
+  double intent_p3;     /*!< 3rd intent parameter. */   /*  96 */
+  double pixdim[8];     /*!< Grid spacings.        */   /* 104 */
+  long long vox_offset; /*!< Offset into .nii file */   /* 168 */
+  double scl_slope;     /*!< Data scaling: slope.  */   /* 176 */
+  double scl_inter;     /*!< Data scaling: offset. */   /* 184 */
+  double cal_max;       /*!< Max display intensity */   /* 192 */
+  double cal_min;       /*!< Min display intensity */   /* 200 */
+  double slice_duration;/*!< Time for 1 slice.     */   /* 208 */
+  double toffset;       /*!< Time axis shift.      */   /* 216 */
+  long long slice_start;/*!< First slice index.    */   /* 224 */
+  long long slice_end;  /*!< Last slice index.     */   /* 232 */
+  char descrip[80];     /*!< any text you like.    */   /* 240 */
+  char aux_file[24];    /*!< auxiliary filename.   */   /* 320 */
+  int qform_code;       /*!< NIFTI_XFORM_* code.   */   /* 344 */
+  int sform_code;       /*!< NIFTI_XFORM_* code.   */   /* 348 */
+  double quatern_b;     /*!< Quaternion b param.   */   /* 352 */
+  double quatern_c;     /*!< Quaternion c param.   */   /* 360 */
+  double quatern_d;     /*!< Quaternion d param.   */   /* 368 */
+  double qoffset_x;     /*!< Quaternion x shift.   */   /* 376 */
+  double qoffset_y;     /*!< Quaternion y shift.   */   /* 384 */
+  double qoffset_z;     /*!< Quaternion z shift.   */   /* 392 */
+  double srow_x[4];     /*!< 1st row affine transform. */ /* 400 */
+  double srow_y[4];     /*!< 2nd row affine transform. */ /* 432 */
+  double srow_z[4];     /*!< 3rd row affine transform. */ /* 464 */
+  int slice_code;       /*!< Slice timing order.   */   /* 496 */
+  int xyzt_units;       /*!< Units of pixdim[1..4] */   /* 500 */
+  int intent_code;      /*!< NIFTI_INTENT_* code.  */   /* 504 */
+  char intent_name[16]; /*!< 'name' or meaning of data. */  /* 508 */
+  char dim_info;        /*!< MRI slice ordering.   */   /* 524 */
+  char unused_str[15];  /*!< unused, filled with \0 */  /* 525 */
+};                    /**** 540 bytes total ****/
+
+typedef struct nifti_2_header nifti_2_header;
+
+/*=================*/
+#ifdef  __cplusplus
+}
+#endif
+/*=================*/
+
+#endif /* __NIFTI2_HEADER */
 // VTK-HeaderTest-Exclude: vtkNIFTIPrivate.h
