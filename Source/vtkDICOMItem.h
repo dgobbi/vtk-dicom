f765320ba07f122e818ddad053bbe1ff8d72db8d
David Gobbi <david.gobbi@gmail.com>
2015-09-09 16:52:55 -0600

    Minor style fix, add braces.
    The code had an assignment within parentheses, which made it look
    like the "=" was meant to be "==".

--- Source/vtkDICOMItem.h
+++ Source/vtkDICOMItem.h
@@ -89,7 +89,7 @@ public:
   //! Clear the data.
   void Clear() {
     if (this->L && --(this->L->ReferenceCount) == 0) { this->FreeList(); }
-    else (this->L = 0); }
+    else { this->L = 0; } }
 
   //! Check if empty.
   bool IsEmpty() const { return (this->L == 0); }
