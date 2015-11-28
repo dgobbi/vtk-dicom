e17a29019457024d8dfe5bc657f89337b174e02f
David Gobbi <david.gobbi@gmail.com>
2015-11-28 09:57:13 -0700

    Centralize the conversion to Windows filenames.

--- Source/vtkDICOMFile.cxx
+++ Source/vtkDICOMFile.cxx
@@ -85,15 +85,9 @@ vtkDICOMFile::vtkDICOMFile(const char *filename, Mode mode)
   this->Error = 0;
   this->Eof = false;
 
-  WCHAR *wideFilename = 0;
-  int n = MultiByteToWideChar(
-    CP_UTF8, MB_ERR_INVALID_CHARS, filename, -1, NULL, 0);
-  if (n > 0)
+  WCHAR *wideFilename = vtkDICOMFile::ConvertToWideChar(filename);
+  if (wideFilename)
     {
-    wideFilename = new WCHAR[n];
-    n = MultiByteToWideChar(
-      CP_UTF8, MB_ERR_INVALID_CHARS, filename, -1, wideFilename, n);
-
     if (mode == In)
       {
       this->Handle = CreateFileW(wideFilename,
@@ -383,14 +377,9 @@ int vtkDICOMFile::Remove(const char *filename)
 {
 #if defined(VTK_DICOM_WIN32_IO)
   int errorCode = 0;
-  WCHAR *wideFilename = 0;
-  int n = MultiByteToWideChar(
-    CP_UTF8, MB_ERR_INVALID_CHARS, filename, -1, NULL, 0);
-  if (n > 0)
+  wchar_t *wideFilename = vtkDICOMFile::ConvertToWideChar(filename);
+  if (wideFilename)
     {
-    wideFilename = new WCHAR[n];
-    n = MultiByteToWideChar(
-      CP_UTF8, MB_ERR_INVALID_CHARS, filename, -1, wideFilename, n);
     if (!DeleteFileW(wideFilename))
       {
       DWORD lastError = GetLastError();
@@ -440,3 +429,25 @@ int vtkDICOMFile::Remove(const char *filename)
   return errorCode;
 #endif
 }
+
+//----------------------------------------------------------------------------
+#if defined(VTK_DICOM_WIN32_IO)
+wchar_t *vtkDICOMFile::ConvertToWideChar(const char *filename)
+{
+  wchar_t *wideFilename = 0;
+  int n = MultiByteToWideChar(
+    CP_UTF8, MB_ERR_INVALID_CHARS, filename, -1, NULL, 0);
+  if (n > 0)
+    {
+    wideFilename = new wchar_t[n];
+    n = MultiByteToWideChar(
+      CP_UTF8, MB_ERR_INVALID_CHARS, filename, -1, wideFilename, n);
+    if (n == 0)
+      {
+      delete [] wideFilename;
+      wideFilename = 0;
+      }
+    }
+  return wideFilename;
+}
+#endif
