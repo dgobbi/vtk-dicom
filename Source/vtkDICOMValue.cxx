40b1ffab729949b99c989a1d797e0aad29fc5036
David Gobbi <david.gobbi@gmail.com>
2021-06-17 12:04:25 -0600

    Fix bounds warning in value code

--- Source/vtkDICOMValue.cxx
+++ Source/vtkDICOMValue.cxx
@@ -1549,13 +1549,12 @@ void vtkDICOMValue::GetValuesT(VT *v, size_t c, size_t s) const
     {
       const vtkDICOMTag *tptr =
         static_cast<const ValueT<vtkDICOMTag> *>(this->V)->Data + s;
-      for (size_t i = 0; i < c; i += 2)
+      for (size_t i = 0; i < c; i++)
       {
-        v[i] = tptr[i/2].GetGroup();
-        v[i+1] = tptr[i/2].GetElement();
+        v[i] = ((i%2) == 0 ? tptr[i/2].GetGroup() : tptr[i/2].GetElement());
       }
-    }
       break;
+    }
   }
 }
 
